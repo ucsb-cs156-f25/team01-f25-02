33821bbdfac419df2d2e2893028f4335c10678a0
Jeffrey Keem <keemaje5@gmail.com>
2025-10-28 18:18:11 -0700

    jeff496 - add test for PUT in recommendation request controller

--- src/test/java/edu/ucsb/cs156/example/controllers/RecommendationRequestControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/RecommendationRequestControllerTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import edu.ucsb.cs156.example.ControllerTestCase;
@@ -23,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -200,5 +202,100 @@ public class RecommendationRequestControllerTests extends ControllerTestCase {
     Map<String, Object> json = responseToJson(response);
     assertEquals("EntityNotFoundException", json.get("type"));
     assertEquals("RecommendationRequest with id 7 not found", json.get("message"));
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void admin_can_edit_an_existing_recommendation_request() throws Exception {
+    // arrange
+
+    ZonedDateTime zdt1 = ZonedDateTime.parse("2022-01-03T00:00:00Z");
+    ZonedDateTime zdt2 = ZonedDateTime.parse("2023-01-04T00:00:00Z");
+    ZonedDateTime zdt3 = ZonedDateTime.parse("2022-01-05T00:00:00Z");
+    ZonedDateTime zdt4 = ZonedDateTime.parse("2023-01-06T00:00:00Z");
+
+    RecommendationRequest recommendationRequest1 =
+        RecommendationRequest.builder()
+            .requesterEmail("requester@gmail.com")
+            .professorEmail("professor@gmail.com")
+            .explanation("Please")
+            .dateRequested(zdt1)
+            .dateNeeded(zdt2)
+            .done(true)
+            .build();
+
+    RecommendationRequest editedRecommendationRequest =
+        RecommendationRequest.builder()
+            .requesterEmail("requesterGuy@gmail.com")
+            .professorEmail("professorGirl@gmail.com")
+            .explanation("Please!")
+            .dateRequested(zdt3)
+            .dateNeeded(zdt4)
+            .done(false)
+            .build();
+
+    String requestBody = mapper.writeValueAsString(editedRecommendationRequest);
+
+    when(recommendationRequestRepository.findById(eq(67L)))
+        .thenReturn(Optional.of(recommendationRequest1));
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                put("/api/recommendationrequest?id=67")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("utf-8")
+                    .content(requestBody)
+                    .with(csrf()))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    verify(recommendationRequestRepository, times(1)).findById(67L);
+    verify(recommendationRequestRepository, times(1))
+        .save(editedRecommendationRequest); // should be saved with correct user
+    String responseString = response.getResponse().getContentAsString();
+    assertEquals(requestBody, responseString);
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void admin_cannot_edit_recommendation_request_that_does_not_exist() throws Exception {
+    // arrange
+
+    ZonedDateTime zdt1 = ZonedDateTime.parse("2022-01-03T00:00:00Z");
+    ZonedDateTime zdt2 = ZonedDateTime.parse("2023-01-04T00:00:00Z");
+
+    RecommendationRequest recommendationRequest1 =
+        RecommendationRequest.builder()
+            .requesterEmail("requester@gmail.com")
+            .professorEmail("professor@gmail.com")
+            .explanation("Please")
+            .dateRequested(zdt1)
+            .dateNeeded(zdt2)
+            .done(true)
+            .build();
+
+    String requestBody = mapper.writeValueAsString(recommendationRequest1);
+
+    when(recommendationRequestRepository.findById(eq(67L))).thenReturn(Optional.empty());
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                put("/api/recommendationrequest?id=67")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("utf-8")
+                    .content(requestBody)
+                    .with(csrf()))
+            .andExpect(status().isNotFound())
+            .andReturn();
+
+    // assert
+    verify(recommendationRequestRepository, times(1)).findById(67L);
+    Map<String, Object> json = responseToJson(response);
+    assertEquals("RecommendationRequest with id 67 not found", json.get("message"));
   }
 }
